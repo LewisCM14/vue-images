9b4c466b9c7932844365917448879e2e9068a0f3
LewisCM14 <Lewiscmartin@live.co.uk>
2022-12-29 17:05:09 +0000

    handle logout functionality

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,21 +1,38 @@
 <template>
     <div class="ui secondary pointing menu">
         <a href="/" class="active item"> Image Storage </a>
+
         <div class="right menu">
-            <a href="#" class="ui item" @click="login">Login</a>
+            <div v-if="isLoggedIn" class="horizontal">
+                <a class="item">Galleries</a>
+                <a class="item">Upload</a>
+                <a class="item" @click="logout">Logout</a>
+            </div>
+
+            <div v-else>
+                <a href="#" class="ui item" @click="login">Login</a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
-import { mapActions } from "vuex";
+import { mapActions, mapGetters } from "vuex";
 
 export default {
     name: "AppHeader",
+    computed: {
+        ...mapGetters(["isLoggedIn"]),
+    },
     methods: {
-        ...mapActions(["login"]),
+        ...mapActions(["login", 'logout',]),
     },
 };
 </script>
 
-<style></style>
+<style scoped>
+.horizontal {
+    display: flex;
+    flex-direction: row;
+}
+</style>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,8 +1,10 @@
 import api from "../../api/imgur";
 import qs from 'qs';
+import { router } from '../../main'
 
 const state = {
-    token: null,
+    // allows login status to persist via local storage, evaluates to null if none stored
+    token: window.localStorage.getItem('imgur_token'),
 };
 
 const getters = {
@@ -16,11 +18,15 @@ const actions = {
     finalizeLogin({ commit }, hash) {
         // collects the key defined after the hash in the url upon login via imgur api
         const query = qs.parse(hash.replace('#', ''));
-        // updates the token to the collected key
+        // updates the token to the collected key & stores for login persistence
         commit('setToken', query.access_token)
+        window.localStorage.setItem('imgur_token', query.access_token)
+        // navigate to home page without app reload
+        router.push('/')
     },
     logout: ({ commit }) => {
         commit("setToken", null);
+        window.localStorage.removeItem('imgur_token')
     },
 };
 
