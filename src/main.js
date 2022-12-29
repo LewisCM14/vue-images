7239421d9bb1bda62a329b5de5c99a2b3245b5a7
LewisCM14 <Lewiscmartin@live.co.uk>
2022-12-29 15:02:40 +0000

    initiate vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,26 @@
 import { createApp } from "vue";
+import { createRouter, createWebHistory } from 'vue-router'
 import App from "./App.vue";
 import store from "./store/index";
+import AuthHandler from './components/AuthHandler'
 
-createApp(App).use(store).mount('#app');
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: App
+    },
+    {
+        path: '/oauth2/callback',
+        name: 'auth',
+        component: AuthHandler,
+    }
+]
+
+const router = createRouter({
+    mode: 'history',
+    history: createWebHistory(),
+    routes
+})
+
+createApp(App).use(store, router).mount('#app');
