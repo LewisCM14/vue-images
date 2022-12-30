8e311da969fda7b37294f745d4b351dc0c63f24e
LewisCM14 <Lewiscmartin@live.co.uk>
2022-12-30 15:23:26 +0000

    handle making a GET request to the imgur images api

--- src/api/imgur.js
+++ src/api/imgur.js
@@ -1,7 +1,8 @@
 import qs from "qs";
+import axios from "axios";
 
 const CLIENT_ID = "";
-const ROOT_URL = "https://api.imgur.com";
+const ROOT_URL = 'https://api.imgur.com';
 
 export default {
     login() {
@@ -12,5 +13,12 @@ export default {
         window.location = `${ROOT_URL}/oauth2/authorize?${qs.stringify(
             querystring
         )}`;
+    },
+    fetchImages(token) {
+        return axios.get(`${ROOT_URL}/3/account/me/images`, {
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        });
     },
 };
